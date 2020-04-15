105e37652a4db80ad02a0573a953ea17bc18bb59
Maxim Alekseev <piratmax@gmail.com>
2020-04-15 21:14:46 +0300

    created AuthViewController Screen

---
+++ BlackStarShop/UIButton+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIButton+Extension.swift
+//  BlackStarShop
+//
+//  Created by Maxim Alekseev on 15.04.2020.
+//  Copyright © 2020 Maxim Alekseev. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    convenience init(title: String,
+                     backgroundColor: UIColor,
+                     titleColor: UIColor,
+                     font: UIFont? = .sfProDisplay15,
+                     cornerRadius: CGFloat = 4) {
+        self.init (type: .system)
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.titleLabel?.font = font
+        self.layer.cornerRadius = cornerRadius
+        
+    }
+}
---
+++ BlackStarShop/UIFont+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+Extension.swift
+//  BlackStarShop
+//
+//  Created by Maxim Alekseev on 15.04.2020.
+//  Copyright © 2020 Maxim Alekseev. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static var helvetica36: UIFont? {
+        return UIFont.init(name: "Helvetica Bold", size: 36)
+    }
+    static var sfProDisplay15: UIFont? {
+        return UIFont.init(name: "SF Pro Display", size: 15)
+    }
+    
+}
