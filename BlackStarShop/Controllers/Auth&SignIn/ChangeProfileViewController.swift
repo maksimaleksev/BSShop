254a980035382f1bb671c316c7a1fe923591fa81
Maxim Alekseev <piratmax@gmail.com>
2020-05-14 14:53:27 +0300

    --

--- BlackStarShop/Controllers/Auth&SignIn/ChangeProfileViewController.swift
+++ BlackStarShop/Controllers/Auth&SignIn/ChangeProfileViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ProfileViewController.swift
+//  ChangeProfileViewController.swift
 //  BlackStarShop
 //
-//  Created by Maxim Alekseev on 20.04.2020.
+//  Created by Maxim Alekseev on 10.05.2020.
 //  Copyright © 2020 Maxim Alekseev. All rights reserved.
 //
 
 import UIKit
-import SwiftUI
-import FirebaseAuth
 import SDWebImage
 
-class SetupProfileViewController: UIViewController {
+class ChangeProfileViewController: UIViewController {
     
-    let createProfileLabel = UILabel(text: "Создайте профиль", font: .sfProDisplay18, textColor: #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1))
+    let changeProfileLabel = UILabel(text: "Отредактируйте профиль", font: .sfProDisplay18, textColor: #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1))
     let profileImageView = AddPhotoView()
     let nameLabel = UILabel(text: "Имя:", font: .sfProDisplay15, textColor: #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1))
     let nameTextField = FormTextField(font: .sfProDisplay15)
@@ -23,15 +21,21 @@ class SetupProfileViewController: UIViewController {
     let cityTextField = FormTextField(font: .sfProDisplay15)
     let addressLabel = UILabel(text: "Адрес:", font: .sfProDisplay15, textColor: #colorLiteral(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1))
     let addressTextField = FormTextField(font: .sfProDisplay15)
-    let createProfileButton = UIButton(title: "Создать профиль",
+    let changeProfileButton = UIButton(title: "Изменить профиль",
                                        backgroundColor: #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1), titleColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
                                        font: .sfProDisplay15, cornerRadius: 24)
     
-    private var currentUser: User
+    private var currentUser: MUser
     
-    init(currentUser: User) {
+    init(currentUser: MUser) {
         self.currentUser = currentUser
         super.init(nibName: nil, bundle: nil)
+        
+        self.nameTextField.text = currentUser.name
+        self.secondNameTextField.text = currentUser.secondName
+        self.cityTextField.text = currentUser.city
+        self.addressTextField.text = currentUser.address
+        self.profileImageView.circleImageView.sd_setImage(with: URL(string: currentUser.avatarStringURL))
     }
     
     required init?(coder: NSCoder) {
@@ -42,17 +46,17 @@ class SetupProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createProfileLabel.textAlignment = .center
+        changeProfileLabel.textAlignment = .center
         view.backgroundColor = .white
         setupConstraints()
-        createProfileButton.addTarget(self, action: #selector(createProfileButtonTapped), for: .touchUpInside)
+        changeProfileButton.addTarget(self, action: #selector(changeProfileButtonTapped), for: .touchUpInside)
         profileImageView.plusButton.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
         
     }
     
-    @objc private func createProfileButtonTapped() {
-        FirestoreService.shared.saveProfileWith(id: currentUser.uid,
-                                                email: currentUser.email!,
+    @objc private func changeProfileButtonTapped() {
+        FirestoreService.shared.saveProfileWith(id: currentUser.id,
+                                                email: currentUser.email,
                                                 name: nameTextField.text,
                                                 avatarImage: profileImageView.circleImageView.image,
                                                 secondName: secondNameTextField.text,
@@ -60,7 +64,7 @@ class SetupProfileViewController: UIViewController {
                                                 address: addressTextField.text) { (result) in
                                                     switch result {
                                                     case .success(let mUser):
-                                                        self.showAlert(with: "Успешно", and: "Приятных вам покупок!", completion: { [weak self] in
+                                                        self.showAlert(with: "Вы успешно изменили профиль", and: "Приятных вам покупок!", completion: { [weak self] in
                                                             let mainTabBarController = MainTabBarController(currentUser: mUser)
                                                             mainTabBarController.modalPresentationStyle = .fullScreen
                                                             self?.present(mainTabBarController, animated: true)
@@ -79,16 +83,16 @@ class SetupProfileViewController: UIViewController {
         imagePickerController.sourceType = .photoLibrary
         present(imagePickerController, animated: true)
     }
-
+    
     
 }
 
-extension SetupProfileViewController {
+extension ChangeProfileViewController {
     
     func setupConstraints() {
         
         let stackView = UIStackView(arrangedSubviews: [nameLabel, nameTextField, secondNameLabel, secondNameTextField, cityLabel, cityTextField, addressLabel, addressTextField], axis: .vertical, spacing: 8)
-        let loginVCElements = [createProfileLabel, stackView, profileImageView, createProfileButton]
+        let loginVCElements = [changeProfileLabel, stackView, profileImageView, changeProfileButton]
         loginVCElements.forEach { [weak self](element) in
             if let self = self {
                 element.translatesAutoresizingMaskIntoConstraints = false
@@ -105,23 +109,23 @@ extension SetupProfileViewController {
         }
         
         NSLayoutConstraint.activate([
-            createProfileLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 56),
-            createProfileLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            profileImageView.topAnchor.constraint(equalTo: createProfileLabel.bottomAnchor, constant: 6),
+            changeProfileLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 56),
+            changeProfileLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImageView.topAnchor.constraint(equalTo: changeProfileLabel.bottomAnchor, constant: 6),
             profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 1),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 27),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -27),
-            createProfileButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 48),
-            createProfileButton.heightAnchor.constraint(equalToConstant: 48),
-            createProfileButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            createProfileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+            changeProfileButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 48),
+            changeProfileButton.heightAnchor.constraint(equalToConstant: 48),
+            changeProfileButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            changeProfileButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
         ])
     }
 }
 
 // MARK: - UIImagePickerControllerDelegate
-extension SetupProfileViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+extension ChangeProfileViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true)
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
